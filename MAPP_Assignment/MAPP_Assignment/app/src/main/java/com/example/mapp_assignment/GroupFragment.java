19a15a0996d384cdac0434bbeab86c55c1181ad0
Zion Jiam <zionjiam88.18@gmail.com>
2019-12-31 22:38:13 +0800

    Update FragementGroup.java
    Added Top Section of Group Page Design

--- MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/GroupFragment.java
+++ MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/GroupFragment.java
@@ -1,18 +1,89 @@
 package com.example.mapp_assignment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mapp_assignment.adapters.HorizontalRecyclerViewAdapter;
+import com.example.mapp_assignment.adapters.YourGroupRecyclerVIewAdapter;
+
+import java.util.ArrayList;
 
 public class GroupFragment extends Fragment {
+
+    private static final String TAG = "GroupFragment";
+    View rootView;
+
+    // temp vars, insert firebase values to this arrayList
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_group,container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_group,container, false);
+
+//        TextView textView = (TextView) rootView.findViewById(R.id.text_view_create_group);
+//        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_add_black_24dp, 0, 0, 0);
+
+        // Display Popular Events through RecyclerView
+        getImages();
+
+        return rootView;
     }
+
+    private void getImages(){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("Cat Gang Lovers");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("TODAY 11:00 PM");
+
+//        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+//        mNames.add("TODAY 11:00 PM");
+//
+//        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+//        mNames.add("TODAY 11:00 PM");
+//
+//
+//        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+//        mNames.add("TODAY 11:00 PM");
+//
+//        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
+//        mNames.add("TODAY 11:00 PM");
+//
+//
+//        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+//        mNames.add("TODAY 11:00 PM");
+//
+//        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+//        mNames.add("TODAY 11:00 PM");
+//
+//        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+//        mNames.add("TODAY 11:00 PM");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = rootView.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(layoutManager);
+        YourGroupRecyclerVIewAdapter adapter = new YourGroupRecyclerVIewAdapter(getContext(), mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+    }
+
 }
---
+++ MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/adapters/YourGroupRecyclerVIewAdapter.java
@@ -0,0 +1,76 @@
+package com.example.mapp_assignment.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.mapp_assignment.R;
+
+import java.util.ArrayList;
+
+public class YourGroupRecyclerVIewAdapter extends RecyclerView.Adapter<YourGroupRecyclerVIewAdapter.ViewHolder> {
+
+    private static final String TAG = "HorizontalRecyclerViewAdapter";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private Context mContext;
+
+    public YourGroupRecyclerVIewAdapter(Context context, ArrayList<String> names, ArrayList<String> imageUrls) {
+        mNames = names;
+        mImageUrls = imageUrls;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.group_your_group_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImageUrls.get(position))
+                .into(holder.image);
+
+        holder.name.setText(mNames.get(position));
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on an image: " + mNames.get(position));
+                Toast.makeText(mContext, mNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageUrls.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+        TextView name;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image_view_group_image);
+            name = itemView.findViewById(R.id.text_view_group_name);
+        }
+    }
+}
