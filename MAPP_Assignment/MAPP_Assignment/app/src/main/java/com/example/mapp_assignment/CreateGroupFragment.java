a099d8cb14f23b59934f4609ce689a299446fe2a
Zion Jiam <zionjiam88.18@gmail.com>
2020-01-17 23:09:48 +0800

    Update CreateGroupFragment.java

--- MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/CreateGroupFragment.java
+++ MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/CreateGroupFragment.java
@@ -14,6 +14,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -25,7 +27,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.example.mapp_assignment.models.Group;
 
-public class CreateGroupFragment extends Fragment{
+public class CreateGroupFragment extends Fragment {
 
     private static final String TAG = "CreateGroupFragment";
     private final String defaultProfileImageUrl = "https://firebasestorage.googleapis.com/v0/b/crux-23cf1.appspot.com/o/default%2Fdefault_proifle_img.jpg?alt=media&token=9e65875e-c926-402f-8cbe-2ef69cc50ce5";
@@ -44,109 +46,114 @@ public class CreateGroupFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_create_group,container, false);
-
+        rootView = inflater.inflate(R.layout.fragment_create_group, container, false);
+        
         textGroupName = rootView.findViewById(R.id.text_input_group_name);
         textGroupDescription = rootView.findViewById(R.id.text_input_group_description);
         spinnerGroupCategory = rootView.findViewById(R.id.group_category);
         textGroupInterest = rootView.findViewById(R.id.text_input_group_interest);
-
         createGroup = rootView.findViewById(R.id.button_create_group);
 
-        // Get instance of firebase authentication
-        fAuth = FirebaseAuth.getInstance();
-        // Get instance of firebase firestore
-        fStore = FirebaseFirestore.getInstance();
-
-
-        // Create ArrayAdapter and set items and layout for spinner
-        ArrayAdapter<String>myAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.category));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerGroupCategory.setAdapter(myAdapter);
-
-        //Create group button
-        createGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createGroup(v);
-            }
-        });
-
+        initFirebaseConnection();
+        initArrayAdapter();
+        initOnClickListener();
 
         return rootView;
     }
 
+    private void initFirebaseConnection(){
+        // Get instance of firebase authentication
+        fAuth = FirebaseAuth.getInstance();
+        // Get instance of firebase firestore
+        fStore = FirebaseFirestore.getInstance();
+    }
 
-    private boolean validateGroupName(){
+    private void initArrayAdapter(){
+        // Create ArrayAdapter and set items and layout for spinner
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.category));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGroupCategory.setAdapter(myAdapter);
+    }
+
+    private void initOnClickListener(){
+        //Create group button
+        createGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!validateGroupName() | !validateGroupDescription()) {
+                    return;
+                } else {
+                    createGroup(v);
+                }
+            }
+        });
+    }
+
+
+    private boolean validateGroupName() {
         String groupNameInput = textGroupName.getEditText().getText().toString().trim();
 
-        if(groupNameInput.isEmpty()){
+        if (groupNameInput.isEmpty()) {
             textGroupName.setError("Please enter a group name");
             return false;
-        }else{
+        } else {
             textGroupName.setError(null);
             return true;
         }
     }
 
-    private boolean validateGroupDescription(){
+    private boolean validateGroupDescription() {
         String groupDescriptionInput = textGroupDescription.getEditText().getText().toString().trim();
 
-        if(groupDescriptionInput.isEmpty()){
+        if (groupDescriptionInput.isEmpty()) {
             textGroupName.setError("Please enter a group description");
             return false;
-        }else if(groupDescriptionInput.length() > 50){
+        } else if (groupDescriptionInput.length() > 50) {
             textGroupName.setError("Description too long");
             return false;
-        }
-        else{
+        } else {
             textGroupName.setError(null);
             return true;
         }
     }
 
-    public void createGroup(View v){
-        if(!validateGroupName() | !validateGroupDescription()){
-            return;
-        }else{
-            // Get User id
-            String userId = fAuth.getCurrentUser().getUid();
-            // Get User input fields
-            String groupNameInput = textGroupName.getEditText().getText().toString().trim();
-            String groupDescriptionInput = textGroupDescription.getEditText().getText().toString().trim();
-            String groupCategoryInput  = spinnerGroupCategory.getSelectedItem().toString();
-            String groupInterestInput = textGroupInterest.getEditText().getText().toString().trim();
-            // Declare and initialize Group object
-            Group newGroup = new Group();
-            newGroup.setCreatorId(userId);
-            newGroup.setGroupMemberCount(0);
-            newGroup.setGroupName(groupNameInput);
-            newGroup.setGroupDescription(groupDescriptionInput);
-            newGroup.setImageURL(defaultProfileImageUrl);
-            newGroup.setCategory(groupCategoryInput);
-            newGroup.setInterest(groupInterestInput);
-            // Add group object to collection
-            CollectionReference grpCollection = fStore.collection("groups");
-            grpCollection.add(newGroup)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Log.d(TAG, "DocumentSnapshot written with ID" + documentReference.getId());
-                            Fragment selectedFragment = new GroupFragment();
-                            getActivity().getSupportFragmentManager().beginTransaction()
-                                    .replace(R.id.fragment_container, selectedFragment, "findThisFragment")
-                                    .addToBackStack(null)
-                                    .commit();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w(TAG, "Error adding document", e);
-                        }
-                    });
-        }
-
+    public void createGroup(View v) {
+        // Get User id
+        String userId = fAuth.getCurrentUser().getUid();
+        // Get User input fields
+        String groupNameInput = textGroupName.getEditText().getText().toString().trim();
+        String groupDescriptionInput = textGroupDescription.getEditText().getText().toString().trim();
+        String groupCategoryInput = spinnerGroupCategory.getSelectedItem().toString();
+        String groupInterestInput = textGroupInterest.getEditText().getText().toString().trim();
+        // Declare and initialize Group object
+        Group newGroup = new Group();
+        newGroup.setCreatorId(userId);
+        newGroup.setGroupMemberCount(0);
+        newGroup.setGroupName(groupNameInput);
+        newGroup.setGroupDescription(groupDescriptionInput);
+        newGroup.setImageURL(defaultProfileImageUrl);
+        newGroup.setCategory(groupCategoryInput);
+        newGroup.setInterest(groupInterestInput);
+        // Add group object to collection
+        CollectionReference grpCollection = fStore.collection("groups");
+        grpCollection.add(newGroup)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID" + documentReference.getId());
+                        Fragment selectedFragment = new GroupFragment();
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, selectedFragment, "findThisFragment")
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
 //        String input = "Group name: " + textGroupName.getEditText().getText().toString();
 //        input += "\n";
 //        input +="Grouo Description: " + textGroupDescription.getEditText().getText().toString();
