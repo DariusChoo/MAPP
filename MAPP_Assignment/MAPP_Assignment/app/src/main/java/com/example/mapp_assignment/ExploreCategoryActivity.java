8ee3680652226ed8bce251926ae0599ef2397d75
DariusChoo <Dariuschoo@gmail.com>
2020-01-29 21:21:55 +0800

    unstaged
    unstaged

--- MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/ExploreCategoryActivity.java
+++ MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/ExploreCategoryActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ExploreCategoryActivity extends AppCompatActivity {
+public class ExploreCategoryActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView gridData;
     ImageView imageView;
@@ -17,11 +20,11 @@ public class ExploreCategoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.explore_cat_activity_grid_item);
 
         gridData = findViewById(R.id.griddata);
         imageView = findViewById(R.id.imageView);
+        ImageButton backCat = findViewById(R.id.backCat);
         Intent intent = getIntent();
         String receivedName =  intent.getStringExtra("name");
         Log.d("ExpCatAct","Received name: "+receivedName);
@@ -29,6 +32,7 @@ public class ExploreCategoryActivity extends AppCompatActivity {
 
         gridData.setText(receivedName);
         imageView.setImageResource(receivedImage);
+
         //enable back Button
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -42,8 +46,22 @@ public class ExploreCategoryActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-//    @Override
-//    public void onBackPressed() {
-//        super.onBackPressed();
-//    }
+
+    @Override
+    public void onBackPressed() {
+
+}
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId())
+        {
+            case R.id.backCat:
+                Intent i = new Intent(this, ExploreFragment.class);
+                startActivity(i);
+                break;
+
+        }
+    }
 }
