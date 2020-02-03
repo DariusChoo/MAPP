9f7bf6e0f48635d695afa5c14d1f4c96efad5f5a
DariusChoo <Dariuschoo@gmail.com>
2020-02-03 19:56:45 +0800

    Revert "Merge branch 'Darius' of https://github.com/DariusChoo/MAPP into Darius"
    This reverts commit 041777c748f30485b1706157f16df24e9c4bd3ef, reversing
    changes made to bb734a57d5313b9f067f304f91a4c6e7ad257df0.

--- MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/models/Chat.java
+++ MAPP_Assignment/MAPP_Assignment/app/src/main/java/com/example/mapp_assignment/models/Chat.java
@@ -73,7 +73,7 @@ public class Chat {
         return lastMsg;
     }
 
-    public void setLastMsg(int eventCount) {
+    public void setLastMsg(String lastMsg) {
         this.lastMsg = lastMsg;
     }
 
